884051a5b5af633a2ea1076d6e564deed0f43390
Ashley Giselle Lorenzo <92343168+ashleylozz02@users.noreply.github.com>
2022-06-21 08:32:03 -0400

    Add files via upload

---
+++ easy.sql
@@ -0,0 +1,7 @@
+USE GI;
+INSERT INTO books (title,publishdate,author) 
+VALUES ('Charlotte web','1952','E.B White'),
+('corduroy ','	2019','Don Freeman'),
+('Stone fox','1980','John Ryenolds'),
+('Rainbow fish','1999','Marcus Pfister'),
+('The Little prince','1943','Antonie de Saint-Expery');
