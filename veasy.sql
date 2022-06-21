87f16db93b6d421a30d06839352d04bffde91b92
Ashley Giselle Lorenzo <92343168+ashleylozz02@users.noreply.github.com>
2022-06-21 13:26:18 -0400

    Update veasy.sql

--- veasy.sql
+++ veasy.sql
@@ -3,4 +3,4 @@ INSERT INTO Cars (Model,Make,Year, prices, color)
 VALUES ('A-Class','Mercedes-Benz','2022','51700','Green'),
 ('A3','	Audi','2023','68,700','green'),
 ('A4','Audi','2021','19000','pink');
-
+---This is also suppose part of hard -------
