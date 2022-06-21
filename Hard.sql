27dedf968c07b231bc5aa812763e404374d158e5
Ashley Giselle Lorenzo <92343168+ashleylozz02@users.noreply.github.com>
2022-06-21 08:41:23 -0400

    Add files via upload

---
+++ Hard.sql
@@ -0,0 +1,3 @@
+ALTER TABLE movies2
+ADD Director_First VARCHAR(80),
+ADD Director_Last VARCHAR(80);
---
+++ Hard1.sql
@@ -0,0 +1,12 @@
+INSERT INTO movies2( Title, ReleaseDate, Rating, Director_First, Director_Last) VALUES
+('Spiderman Homecomming','2017','Pg-13','Jon', 'watts'),
+('Spiderman Far From home',	'2019','Pg-13', 'Jon','watts'),
+('Spiderman no way home ','2021','pg-13', 'Jon','watts'),
+('Under the same moon','2007','Pg-13''patricia' , 'Riggen'),
+('Roma','2018','R','Alfonso','cuadro'),
+('Frida','2002','R','Julie','Taymor'),
+('coco','2017','Pg','Adrian','molina'),
+('Tortila soup','2001','pg-13','maria','ripoll'),
+('miss bala','2019','pg-13','catherine','Hardwicke'),
+('Back to the future','1985','pg-13','Robert','zemicks');
+
