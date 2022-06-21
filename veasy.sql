ec6bf572ce4e9c1d733a6cfd78e15a9b7be21639
Ashley Giselle Lorenzo <92343168+ashleylozz02@users.noreply.github.com>
2022-06-21 08:26:15 -0400

    Delete veasy.sql

--- veasy.sql
+++
@@ -1,6 +0,0 @@
-USE GI;
-INSERT INTO Cars (Model,Make,Year, prices, color) 
-VALUES ('A-Class','Mercedes-Benz','2022','51700','Green'),
-('A3','	Audi','2023','68,700','green'),
-('A4','Audi','2021','19000','pink');
-
